d4fe18de407dbc89d40adba6890752809e242fbd
maggiec25 <lamach713@gmail.com>
2024-10-30 17:13:39 +0000

    binary converter

---
+++ binary_converter.py
@@ -0,0 +1,12 @@
+def binaryToDecimal(num):
+    """This function converts decimal number
+    to binary and prints it"""
+    if num > 1:
+        binaryToDecimal(num // 2)
+    print(num % 2, end='')
+
+
+# decimal number
+number = int(input("Enter any decimal number: "))
+
+binaryToDecimal(number)
